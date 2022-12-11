6c6d39e4f7891ad88c9c8df1617afc76da17351b
MariAsenova <mariaasenova22@gmail.com>
2022-12-11 22:47:42 +0100

    add hard coded api endpoints for testing api on deployment

--- app.js
+++ app.js
@@ -2,9 +2,31 @@ const express = require('express');
 const app = express();
 const hostname = "0.0.0.0";
 const port = 3000;
+// apply json middleware for processeing req body
+app.use( express.json() )
 
 app.get("/", (req, res) => {
     res.send("Running Express JS in a Docker container");
+});
+
+app.get('/movie', (req, res) => {
+    res.status(200).send({
+        movie: 'action',
+        title: 'My movie title'
+    })
+});
+
+app.post('/movie/:id', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    if (!title) {
+        res.status(418).send({ message: 'We need a title!' })
+    }
+
+    res.send({
+        movie: `tshirt with ID: ${id} and logo ${title}`,
+    });
 });
 
 app.listen(port, () => {
